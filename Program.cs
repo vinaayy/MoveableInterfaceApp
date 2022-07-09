081d922e0e6c14d12ed90d2c65f6ec972210d64f
vinaayy <80506397+vinaayy@users.noreply.github.com>
2022-07-09 19:05:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoveablePolymorphismApp.Model;
+
+namespace MoveablePolymorphismApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicles[] v = new Vehicles[3];
+            v[0] = new  Car("Tata", 2018, "Tiago");
+            v[1] = new Bike("Honda", 2022, "Activa");
+            v[2] = new Truck("Mahindra", 2010, "BigTruck");
+            StartRace(v);
+        }
+        public static void StartRace(Vehicles[]  movables)
+        {
+            foreach (Vehicles vehicle in movables )
+            {
+                PrintDetails(vehicle);
+                vehicle.Move();
+            }
+        }
+
+        public static void PrintDetails(Vehicles obj)
+        {
+            Console.WriteLine("\nVehicle Details :");
+            Console.WriteLine("Company : "+obj.Company);
+            Console.WriteLine("Year    : "+obj.Year);
+            Console.WriteLine("Model   : "+obj.Model);
+        }
+    }
+}
---
+++ Vehicles.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveablePolymorphismApp.Model
+{
+    public  abstract class Vehicles:IMovable
+    {
+        private string _company;
+        private int _year;
+        private string _model;
+
+        public Vehicles(string company , int year , string model)
+        {
+            _company = company;
+            _year = year;
+            _model = model;
+        }
+        public abstract void Move();
+        public string Company
+        {
+            get { return _company; }
+        }
+        public int Year
+        {
+            get { return _year; }
+        }
+        public string Model
+        {
+            get { return _model; }
+        }
+
+    }
+    
+
+    public class Car : Vehicles
+    {
+        public Car(string company, int year, string model):base(company , year , model)
+        {
+
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine("Car Moving...");
+        }
+    }
+
+    public class Bike : Vehicles
+    {
+        public Bike(string company, int year, string model) : base(company, year, model)
+        {
+
+        }
+        public override void Move()
+        {
+            Console.WriteLine("Bike Moving...");
+        }
+    }
+
+    public class Truck : Vehicles
+    {
+        public Truck(string company, int year, string model) : base(company, year, model)
+        {
+
+        }
+        public override void Move()
+        {
+            Console.WriteLine("Truck Moving...");
+        }
+    }
+}
